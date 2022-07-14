6f22fa3863802e328e56883d6ec786e04fcbb21d
andreyshirokoff <androidadress1@gmail.com>
2022-07-14 14:54:31 +0700

    14.07

---
+++ src/App.tsx
@@ -0,0 +1,26 @@
+import React, { useState } from 'react'
+import './App.css'
+import InputField from './components/InputFiled'
+import TodoList from './components/TodoList'
+import { Todo } from './model'
+
+const App: React.FC = () => {
+	const [todo, setTodo] = useState<string>("")
+	const [todos, setTodos] = useState<Todo[]>([])
+	const handleAdd = (e: React.FormEvent) => {
+		e.preventDefault();
+		if (todo) {
+			setTodos([...todos, { id: Date.now(), todo, isDone: false }])
+			setTodo("")
+		}
+	}
+	// console.log(todos);
+
+	return <div className='App'>
+		<span className='header'>TASKIFY</span>
+		<InputField todo={todo} setTodo={setTodo} handleAdd={handleAdd} />
+		<TodoList todos={todos} setTodos={setTodos} />
+	</div >
+}
+
+export default App; 
